80a5e4e41cb083a1bfe5a8fbd57ca7f8f6986b1b
Thomas Sparks <69657545+thsparks@users.noreply.github.com>
2026-08-14 16:13:34 -0700

    Initial files for MakeCode project

---
+++ custom.ts
@@ -0,0 +1,51 @@
+/**
+ * Custom blocks
+ */
+//% weight=100 color=#0fbc11 icon="" block="AI Gestures"
+namespace gesture_palooza {
+    let sending_data = false;
+    let start_time = 0;
+    const timeoutSec = 60;
+
+    /**
+     * Indicate that a gesture was performed
+     * @param gesture The gesture that was performed
+     * @param icon The icon to show on your microbit
+     * @param sound The sound to play
+     */
+    //% blockId=ai_gestures_send_gesture
+    //% block="send gesture $gesture with icon $icon"
+    export function send_gesture(gesture: string, icon: IconNames): void {
+        if (sending_data) {
+            radio.sendString(gesture)
+            music.play(music.stringPlayable("C C5 - - - - - - ", 320), music.PlaybackMode.InBackground)
+        }
+        basic.showIcon(icon)
+    }
+
+    /**
+     * Begin sending data to the reciever for one minute!
+     */
+    //% blockId=ai_gestures_start_sending_data
+    //% block="start timer"
+    export function start_recording() {
+        music.play(music.tonePlayable(262, music.beat(BeatFraction.Half)), music.PlaybackMode.InBackground)
+        basic.showNumber(3)
+        music.play(music.tonePlayable(262, music.beat(BeatFraction.Half)), music.PlaybackMode.InBackground)
+        basic.showNumber(2)
+        music.play(music.tonePlayable(262, music.beat(BeatFraction.Half)), music.PlaybackMode.InBackground)
+        basic.showNumber(1)
+        music.play(music.tonePlayable(523, music.beat(BeatFraction.Double)), music.PlaybackMode.InBackground)
+        basic.showIcon(IconNames.Yes)
+        start_time = input.runningTime()
+        sending_data = true;
+    }
+
+    loops.everyInterval(1000, function() {
+        if (sending_data && input.runningTime() - start_time > (timeoutSec * 1000)) {
+            sending_data = false;
+            music.play(music.tonePlayable(440, music.beat(BeatFraction.Breve)), music.PlaybackMode.InBackground)
+            basic.showIcon(IconNames.No);
+        }
+    })
+}
